c882721f0619a89de685908205773055e219b3bc
adebayo isreal <adebayoisrealight122@gmail.com>
2024-01-10 15:37:42 +0100

    i created a file

---
+++ scrip.js
@@ -0,0 +1,29 @@
+// let allStudents =["Adebayo", "Gbogunmi", "Lola", "Tanimola", "Orere", "ekundayo"]
+
+
+// allStudents.pop();
+// allStudents.shift();
+// allStudents.push("Opomulero");
+// allStudents.unshift("Precious")
+// console.log(allStudents);
+
+
+let cart = []
+
+function Add() {
+    if(item.value !== ""){
+
+        cart.push(item.value)
+        disp.innerHTML = ""
+        for (let i = 0; i < cart.length; i++) {
+            disp.innerHTML += `<p> ${i + 1}. ${cart[i]} </p>`
+            item.
+        }
+    }else {
+        show.innerHTML = `<p> please fill up the input</p>`
+        setTimeout(() => {
+            show.innerHTML = ""
+        }, 3000);
+    }
+    
+}
